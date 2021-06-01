49cfe1036a2a4c24f95614c43efb82c1380dc6c1
prodhc <zionywood@gmail.com>
2021-05-31 20:01:23 -0700

    server start up

--- api/server.js
+++ api/server.js
@@ -1,3 +1,9 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express()
+
+server.get('/hello', (req, res) => {
+    res.json({ message: 'hello'})
+})
+module.exports = server; // EXPORT YOUR SERVER instead of {}
